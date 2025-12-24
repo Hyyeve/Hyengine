c23955ea8f57983b83d84928f30e16eb676a7ade
Hyeve <34379593+Hyyeve@users.noreply.github.com>
2025-12-24 16:21:24 +0000

    Fix cmake issues and namespace PCG generators

---
+++ sources/hyengine/library/pcg.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <pcg/include/pcg_extras.hpp>
+#include <pcg/include/pcg_uint128.hpp>
+#include <pcg/include/pcg_random.hpp>
--- sources/pcg/include/pcg_random.hpp
+++ sources/pcg/include/pcg_random.hpp
@@ -672,9 +672,8 @@ itype operator-(const engine<xtype,itype,
                                output_mixin,output_previous,
                                stream_mixin_rhs, multiplier_mixin_rhs>& rhs)
 {
-    if (lhs.multiplier() != rhs.multiplier()
-        || lhs.increment() != rhs.increment())
-        throw std::logic_error("incomparable generators");
+    if (lhs.multiplier() != rhs.multiplier() || lhs.increment() != rhs.increment())
+        return itype(-1);
     return rhs.distance(lhs.state_);
 }
 
@@ -1667,85 +1666,88 @@ using ext_setseq_xsl_rr_128_64 =
 
 } // namespace pcg_engines
 
-typedef pcg_engines::setseq_xsh_rr_64_32        pcg32;
-typedef pcg_engines::oneseq_xsh_rr_64_32        pcg32_oneseq;
-typedef pcg_engines::unique_xsh_rr_64_32        pcg32_unique;
-typedef pcg_engines::mcg_xsh_rs_64_32           pcg32_fast;
+namespace pcg
+{
+    typedef pcg_engines::setseq_xsh_rr_64_32        pcg32;
+    typedef pcg_engines::oneseq_xsh_rr_64_32        pcg32_oneseq;
+    typedef pcg_engines::unique_xsh_rr_64_32        pcg32_unique;
+    typedef pcg_engines::mcg_xsh_rs_64_32           pcg32_fast;
 
-typedef pcg_engines::setseq_xsl_rr_128_64       pcg64;
-typedef pcg_engines::oneseq_xsl_rr_128_64       pcg64_oneseq;
-typedef pcg_engines::unique_xsl_rr_128_64       pcg64_unique;
-typedef pcg_engines::mcg_xsl_rr_128_64          pcg64_fast;
+    typedef pcg_engines::setseq_xsl_rr_128_64       pcg64;
+    typedef pcg_engines::oneseq_xsl_rr_128_64       pcg64_oneseq;
+    typedef pcg_engines::unique_xsl_rr_128_64       pcg64_unique;
+    typedef pcg_engines::mcg_xsl_rr_128_64          pcg64_fast;
 
-typedef pcg_engines::setseq_rxs_m_xs_8_8        pcg8_once_insecure;
-typedef pcg_engines::setseq_rxs_m_xs_16_16      pcg16_once_insecure;
-typedef pcg_engines::setseq_rxs_m_xs_32_32      pcg32_once_insecure;
-typedef pcg_engines::setseq_rxs_m_xs_64_64      pcg64_once_insecure;
-typedef pcg_engines::setseq_xsl_rr_rr_128_128   pcg128_once_insecure;
+    typedef pcg_engines::setseq_rxs_m_xs_8_8        pcg8_once_insecure;
+    typedef pcg_engines::setseq_rxs_m_xs_16_16      pcg16_once_insecure;
+    typedef pcg_engines::setseq_rxs_m_xs_32_32      pcg32_once_insecure;
+    typedef pcg_engines::setseq_rxs_m_xs_64_64      pcg64_once_insecure;
+    typedef pcg_engines::setseq_xsl_rr_rr_128_128   pcg128_once_insecure;
 
-typedef pcg_engines::oneseq_rxs_m_xs_8_8        pcg8_oneseq_once_insecure;
-typedef pcg_engines::oneseq_rxs_m_xs_16_16      pcg16_oneseq_once_insecure;
-typedef pcg_engines::oneseq_rxs_m_xs_32_32      pcg32_oneseq_once_insecure;
-typedef pcg_engines::oneseq_rxs_m_xs_64_64      pcg64_oneseq_once_insecure;
-typedef pcg_engines::oneseq_xsl_rr_rr_128_128   pcg128_oneseq_once_insecure;
+    typedef pcg_engines::oneseq_rxs_m_xs_8_8        pcg8_oneseq_once_insecure;
+    typedef pcg_engines::oneseq_rxs_m_xs_16_16      pcg16_oneseq_once_insecure;
+    typedef pcg_engines::oneseq_rxs_m_xs_32_32      pcg32_oneseq_once_insecure;
+    typedef pcg_engines::oneseq_rxs_m_xs_64_64      pcg64_oneseq_once_insecure;
+    typedef pcg_engines::oneseq_xsl_rr_rr_128_128   pcg128_oneseq_once_insecure;
 
 
-// These two extended RNGs provide two-dimensionally equidistributed
-// 32-bit generators.  pcg32_k2_fast occupies the same space as pcg64,
-// and can be called twice to generate 64 bits, but does not required
-// 128-bit math; on 32-bit systems, it's faster than pcg64 as well.
+    // These two extended RNGs provide two-dimensionally equidistributed
+    // 32-bit generators.  pcg32_k2_fast occupies the same space as pcg64,
+    // and can be called twice to generate 64 bits, but does not required
+    // 128-bit math; on 32-bit systems, it's faster than pcg64 as well.
 
-typedef pcg_engines::ext_setseq_xsh_rr_64_32<6,16,true>     pcg32_k2;
-typedef pcg_engines::ext_oneseq_xsh_rs_64_32<6,32,true>     pcg32_k2_fast;
+    typedef pcg_engines::ext_setseq_xsh_rr_64_32<6,16,true>     pcg32_k2;
+    typedef pcg_engines::ext_oneseq_xsh_rs_64_32<6,32,true>     pcg32_k2_fast;
 
-// These eight extended RNGs have about as much state as arc4random
-//
-//  - the k variants are k-dimensionally equidistributed
-//  - the c variants offer better crypographic security
-//
-// (just how good the cryptographic security is is an open question)
+    // These eight extended RNGs have about as much state as arc4random
+    //
+    //  - the k variants are k-dimensionally equidistributed
+    //  - the c variants offer better crypographic security
+    //
+    // (just how good the cryptographic security is is an open question)
 
-typedef pcg_engines::ext_setseq_xsh_rr_64_32<6,16,true>     pcg32_k64;
-typedef pcg_engines::ext_mcg_xsh_rs_64_32<6,32,true>        pcg32_k64_oneseq;
-typedef pcg_engines::ext_oneseq_xsh_rs_64_32<6,32,true>     pcg32_k64_fast;
+    typedef pcg_engines::ext_setseq_xsh_rr_64_32<6,16,true>     pcg32_k64;
+    typedef pcg_engines::ext_mcg_xsh_rs_64_32<6,32,true>        pcg32_k64_oneseq;
+    typedef pcg_engines::ext_oneseq_xsh_rs_64_32<6,32,true>     pcg32_k64_fast;
 
-typedef pcg_engines::ext_setseq_xsh_rr_64_32<6,16,false>    pcg32_c64;
-typedef pcg_engines::ext_oneseq_xsh_rs_64_32<6,32,false>    pcg32_c64_oneseq;
-typedef pcg_engines::ext_mcg_xsh_rs_64_32<6,32,false>       pcg32_c64_fast;
+    typedef pcg_engines::ext_setseq_xsh_rr_64_32<6,16,false>    pcg32_c64;
+    typedef pcg_engines::ext_oneseq_xsh_rs_64_32<6,32,false>    pcg32_c64_oneseq;
+    typedef pcg_engines::ext_mcg_xsh_rs_64_32<6,32,false>       pcg32_c64_fast;
 
-typedef pcg_engines::ext_setseq_xsl_rr_128_64<5,16,true>    pcg64_k32;
-typedef pcg_engines::ext_oneseq_xsl_rr_128_64<5,128,true>   pcg64_k32_oneseq;
-typedef pcg_engines::ext_mcg_xsl_rr_128_64<5,128,true>      pcg64_k32_fast;
+    typedef pcg_engines::ext_setseq_xsl_rr_128_64<5,16,true>    pcg64_k32;
+    typedef pcg_engines::ext_oneseq_xsl_rr_128_64<5,128,true>   pcg64_k32_oneseq;
+    typedef pcg_engines::ext_mcg_xsl_rr_128_64<5,128,true>      pcg64_k32_fast;
 
-typedef pcg_engines::ext_setseq_xsl_rr_128_64<5,16,false>   pcg64_c32;
-typedef pcg_engines::ext_oneseq_xsl_rr_128_64<5,128,false>  pcg64_c32_oneseq;
-typedef pcg_engines::ext_mcg_xsl_rr_128_64<5,128,false>     pcg64_c32_fast;
+    typedef pcg_engines::ext_setseq_xsl_rr_128_64<5,16,false>   pcg64_c32;
+    typedef pcg_engines::ext_oneseq_xsl_rr_128_64<5,128,false>  pcg64_c32_oneseq;
+    typedef pcg_engines::ext_mcg_xsl_rr_128_64<5,128,false>     pcg64_c32_fast;
 
-// These eight extended RNGs have more state than the Mersenne twister
-//
-//  - the k variants are k-dimensionally equidistributed
-//  - the c variants offer better crypographic security
-//
-// (just how good the cryptographic security is is an open question)
+    // These eight extended RNGs have more state than the Mersenne twister
+    //
+    //  - the k variants are k-dimensionally equidistributed
+    //  - the c variants offer better crypographic security
+    //
+    // (just how good the cryptographic security is is an open question)
 
-typedef pcg_engines::ext_setseq_xsh_rr_64_32<10,16,true>    pcg32_k1024;
-typedef pcg_engines::ext_oneseq_xsh_rs_64_32<10,32,true>    pcg32_k1024_fast;
+    typedef pcg_engines::ext_setseq_xsh_rr_64_32<10,16,true>    pcg32_k1024;
+    typedef pcg_engines::ext_oneseq_xsh_rs_64_32<10,32,true>    pcg32_k1024_fast;
 
-typedef pcg_engines::ext_setseq_xsh_rr_64_32<10,16,false>   pcg32_c1024;
-typedef pcg_engines::ext_oneseq_xsh_rs_64_32<10,32,false>   pcg32_c1024_fast;
+    typedef pcg_engines::ext_setseq_xsh_rr_64_32<10,16,false>   pcg32_c1024;
+    typedef pcg_engines::ext_oneseq_xsh_rs_64_32<10,32,false>   pcg32_c1024_fast;
 
-typedef pcg_engines::ext_setseq_xsl_rr_128_64<10,16,true>   pcg64_k1024;
-typedef pcg_engines::ext_oneseq_xsl_rr_128_64<10,128,true>  pcg64_k1024_fast;
+    typedef pcg_engines::ext_setseq_xsl_rr_128_64<10,16,true>   pcg64_k1024;
+    typedef pcg_engines::ext_oneseq_xsl_rr_128_64<10,128,true>  pcg64_k1024_fast;
 
-typedef pcg_engines::ext_setseq_xsl_rr_128_64<10,16,false>  pcg64_c1024;
-typedef pcg_engines::ext_oneseq_xsl_rr_128_64<10,128,false> pcg64_c1024_fast;
+    typedef pcg_engines::ext_setseq_xsl_rr_128_64<10,16,false>  pcg64_c1024;
+    typedef pcg_engines::ext_oneseq_xsl_rr_128_64<10,128,false> pcg64_c1024_fast;
 
-// These generators have an insanely huge period (2^524352), and is suitable
-// for silly party tricks, such as dumping out 64 KB ZIP files at an arbitrary
-// point in the future.   [Actually, over the full period of the generator, it
-// will produce every 64 KB ZIP file 2^64 times!]
+    // These generators have an insanely huge period (2^524352), and is suitable
+    // for silly party tricks, such as dumping out 64 KB ZIP files at an arbitrary
+    // point in the future.   [Actually, over the full period of the generator, it
+    // will produce every 64 KB ZIP file 2^64 times!]
 
-typedef pcg_engines::ext_setseq_xsh_rr_64_32<14,16,true>    pcg32_k16384;
-typedef pcg_engines::ext_oneseq_xsh_rs_64_32<14,32,true>    pcg32_k16384_fast;
+    typedef pcg_engines::ext_setseq_xsh_rr_64_32<14,16,true>    pcg32_k16384;
+    typedef pcg_engines::ext_oneseq_xsh_rs_64_32<14,32,true>    pcg32_k16384_fast;
+}
 
 #endif // PCG_RAND_HPP_INCLUDED
